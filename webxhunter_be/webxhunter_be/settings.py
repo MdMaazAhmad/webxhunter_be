7f163d86cc191ad8d662246bef7572d0c2b99678
mdmaazahmad <mdmaazahmad120@gmail.com>
2025-09-01 12:56:05 +0530

    second

--- webxhunter_be/webxhunter_be/settings.py
+++ webxhunter_be/webxhunter_be/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h$9kf_!h-$!c$+55_fc0-n*i+yyoi^jd(0@ay2%@9=+6i6oo4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
